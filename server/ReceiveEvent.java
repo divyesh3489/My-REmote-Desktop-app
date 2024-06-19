55b3fe274968dcde944aa309ee1111cbf00f58fd
divyesh <divyeshgurjar2004@gmail.com>
2024-06-19 22:26:04 +0530

    final

--- server/ReceiveEvent.java
+++ server/ReceiveEvent.java
@@ -22,26 +22,31 @@ public class ReceiveEvent extends  Thread
         try {
             sc = new Scanner(cs.getInputStream());
             while (this.loopFlag) {
-                int command = sc.nextInt();
-                switch (command) {
-                    case -1:
-                        robot.mousePress(sc.nextInt());
-                        break;
-                    case -2:
-                        robot.mouseRelease(sc.nextInt());
-                        break;
-                    case -3:
-                        robot.keyPress(sc.nextInt());
-                        break;
-                    case -4:
-                        robot.keyRelease(sc.nextInt());
-                        break;
-                    case -5:
-                        robot.mouseMove(sc.nextInt(),sc.nextInt());
-                        break;
-                    default:
-                        throw new AssertionError();
-                } 
+                System.out.println(sc.hasNext());
+                if (sc.hasNext())
+                {
+                    int command = sc.nextInt();
+                    switch (command) {
+                        case -1:
+                            robot.mousePress(sc.nextInt());
+                            break;
+                        case -2:
+                            robot.mouseRelease(sc.nextInt());
+                            break;
+                        case -3:
+                            robot.keyPress(sc.nextInt());
+                            break;
+                        case -4:
+                            robot.keyRelease(sc.nextInt());
+                            break;
+                        case -5:
+                            robot.mouseMove(sc.nextInt(), sc.nextInt());
+                            break;
+                        default:
+                            throw new AssertionError();
+                    }
+                }
+                
                 
             }
         } catch (IOException e) {
