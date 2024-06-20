0da4ac3e74a7d6cbad3de45c1e2fc6875c682bd6
divyesh <divyeshgurjar2004@gmail.com>
2024-06-20 18:31:51 +0530

    voice1

--- server/AudioReceiver.java
+++ server/AudioReceiver.java
@@ -1,32 +1,36 @@
 import javax.sound.sampled.*;
 import java.io.IOException;
-import java.io.OutputStream;
+import java.io.InputStream;
 import java.net.Socket;
 
-public class AudioSender extends Thread {
+public class AudioReceiver extends Thread {
     private Socket socket;
     private boolean running = true;
 
-    public AudioSender(Socket socket) {
-        this.socket = socket;
+    public AudioReceiver(String serverIp, int port) {
+        try {
+            socket = new Socket(serverIp, port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void run() {
         AudioFormat format = getAudioFormat();
-        DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+        DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
 
-        try (TargetDataLine line = (TargetDataLine) AudioSystem.getLine(info);
-                OutputStream outputStream = socket.getOutputStream()) {
+        try (SourceDataLine speakers = (SourceDataLine) AudioSystem.getLine(info);
+             InputStream inputStream = socket.getInputStream()) {
 
-            line.open(format);
-            line.start();
+            speakers.open(format);
+            speakers.start();
 
             byte[] buffer = new byte[4096];
             while (running) {
-                int bytesRead = line.read(buffer, 0, buffer.length);
+                int bytesRead = inputStream.read(buffer, 0, buffer.length);
                 if (bytesRead > 0) {
-                    outputStream.write(buffer, 0, bytesRead);
+                    speakers.write(buffer, 0, bytesRead);
                 }
             }
         } catch (LineUnavailableException | IOException e) {
@@ -43,5 +47,7 @@ public class AudioSender extends Thread {
         return new AudioFormat(sampleRate, sampleSizeInBits, channels, signed, bigEndian);
     }
 
-public void stopRunning() {
+    public void stopRunning() {
         running = false;
+    }
+}
