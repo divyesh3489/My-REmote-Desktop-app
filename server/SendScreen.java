8202eaea73b2c871632f07dfd1faa00c43392799
Dhruvil0037 <dhruvildhamecha2003@gmail.com>
2024-06-22 12:23:53 +0530

    fixing of server side after discconect

--- server/SendScreen.java
+++ server/SendScreen.java
@@ -33,14 +33,22 @@ public class SendScreen extends Thread {
         while(this.loopFlag){
             BufferedImage screenCapture = robot.createScreenCapture(screenRect);
             try {
-                ImageIO.write(screenCapture, "jpeg", output);
-                
+                ImageIO.write(screenCapture, "jpeg", output); 
             } catch (Exception e) {
                 e.printStackTrace();
+                this.loopFlag= false;
             }
             try {
                 Thread.sleep(10);
             } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        if(this.loopFlag == false){
+            try{
+                clientSocket.close();
+            }
+            catch(Exception e){
                 e.printStackTrace();
             }
         }
