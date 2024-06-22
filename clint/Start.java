fe72582742bf233ef8af4efa97bbf11d48a40eca
divyesh <divyeshgurjar2004@gmail.com>
2024-06-22 23:08:31 +0530

    space remove

--- clint/Start.java
+++ clint/Start.java
@@ -26,13 +26,9 @@ public class Start extends JFrame {
     }
     public static void main(String[] args) {
         Start startProgram = new Start();
-   
             String ip = JOptionPane.showInputDialog(null, "Enter the Server IP Address or HostName:", "Server IP", JOptionPane.QUESTION_MESSAGE);
             startProgram.createFrame(ip);
             startProgram.createSocket(ip, Integer.parseInt(port));
-
-       
-
     }
 
     public void  createSocket(String ip, int port) {
